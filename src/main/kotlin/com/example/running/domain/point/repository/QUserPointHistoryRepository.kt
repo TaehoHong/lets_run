abdca32b149e63ccc51600a1fed5952342be6bcb
홍태호 <taeho.h95@gmail.com>
2025-06-27 19:18:03 +0900

    feat: 불필요 클래스 삭제

--- src/main/kotlin/com/example/running/domain/point/repository/QUserPointHistoryRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.running.domain.point.repository
-
-import com.example.running.domain.point.entity.UserPointHistory
-
-interface QUserPointHistoryRepository {
-
-    fun findAll(userId: Long, id: Long?, size: Int): List<UserPointHistory>
-}
