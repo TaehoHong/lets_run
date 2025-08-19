4e2af552a9da1688abeb6fe248a9a5a15e01f05a
홍태호 <taeho.h95@gmail.com>
2025-08-19 15:03:44 +0900

    feat: 러닝 종료 API Security 설정 변경

--- src/main/kotlin/com/example/running/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/running/security/SecurityConfig.kt
@@ -60,6 +60,7 @@ class SecurityConfig(
 
                 authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/api/v1/running").authenticated()
                 authorizeHttpRequests.requestMatchers(HttpMethod.PUT, "/api/v1/running/*").authenticated()
+                authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/api/v1/running/*/end").authenticated()
                 authorizeHttpRequests.requestMatchers(HttpMethod.GET, "/api/v1/running").authenticated()
                 authorizeHttpRequests.requestMatchers(HttpMethod.GET, "/api/v1/running/statistics").authenticated()
 
